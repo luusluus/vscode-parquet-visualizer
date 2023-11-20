a00fc5d34ad42a76a3252dddaae34be5b4a1732b
luus <lucien@moproblems.nl>
2023-11-20 23:40:18 +0100

    WIP new parquet lib. Reading works.

--- src/parquet-paginator.ts
+++ src/parquet-paginator.ts
@@ -3,26 +3,37 @@
 // npx ts-node parquet.ts 
 // import * as readline from 'readline/promises';
 
-import { ParquetReader, ParquetSchema } from '@dvirtz/parquets';
+import * as fs from 'fs';
+
+import {tableFromIPC, Table, Schema} from "apache-arrow";
+import {
+    readParquet,
+} from "parquet-wasm/node/arrow1";
+
 
 export class ParquetPaginator {
-  private reader: ParquetReader<unknown>;
+  private table: Table<any>;
   private pageSize: number;
   private rowCount: number;
   private pageCount: number;
-  private schema: ParquetSchema;
+  private schema: Schema;
 
-  private constructor(reader: ParquetReader<unknown>, pageSize: number) {
-    this.reader = reader;
+  private constructor(table: Table<any>, pageSize: number) {
+    this.table = table;
+    this.schema = table.schema;
+    this.rowCount = this.table.numRows;
     this.pageSize = pageSize;
-    this.schema = this.reader.getSchema();
-    this.rowCount = this.reader.getRowCount();
     this.pageCount = Math.ceil(this.rowCount / this.pageSize);
   }
 
   public static async createAsync (filePath: string, pageSize: number = 10) {
-    const reader = await ParquetReader.openFile(filePath);
-    return new ParquetPaginator(reader, pageSize);
+    const byteArray = fs.readFileSync(filePath);
+    const arrowUint8Array = readParquet(byteArray);
+    const arrowTable = tableFromIPC(arrowUint8Array.intoIPCStream());
+
+    console.log(arrowTable);
+
+    return new ParquetPaginator(arrowTable, pageSize);
   }
 
   // TODO: return a complex type called Page, with a method to get only values, not keys
@@ -35,32 +46,41 @@ export class ParquetPaginator {
     let startIndex = (pageNumber - 1) * this.pageSize;
     let endIndex = Math.min(startIndex + this.pageSize - 1, this.rowCount);
 
-    let rows = [];
-    const cursor = this.reader.getCursor();
-    for (let i = 0; i < this.rowCount; i++) {
-      // get subset of rows based on startindex and endindex
-      const row = await cursor.next();
-      if (i >= startIndex && i <= endIndex) {
-        rows.push(row);
-      }
-      if (i > endIndex) {
-        break;
-      }
-    }
+    // let rows = [];
+    // console.log(this.table.toArray());
+    const subTable = this.table.slice(startIndex, endIndex);
+    const rows = subTable.toArray().map(r => Object.values(r));
+    console.log(subTable.toArray()[0]);
+    console.log(Object.values(rows[0]));
+    // console.log(rows[0]);
+    
+    // console.log(rows[0].values());
+    // console.log(this.table.slice(startIndex, endIndex).data);
+    // const cursor = this.reader.getCursor();
+    // for (let i = 0; i < this.rowCount; i++) {
+    //   // get subset of rows based on startindex and endindex
+    //   const row = await cursor.next();
+    //   if (i >= startIndex && i <= endIndex) {
+    //     rows.push(row);
+    //   }
+    //   if (i > endIndex) {
+    //     break;
+    //   }
+    // }
 
     return rows;
   }
 
   public getSchema() {
-    return this.schema.schema;
+    // return this.schema.schema;
   }
 
   public getFieldList() {
-    return this.schema.fieldList;
+    // return this.schema.fieldList;
   }
 
   public getFields() {
-    return this.schema.fields;
+    // return this.schema.fields;
   }
 
   public getPageSize() {
