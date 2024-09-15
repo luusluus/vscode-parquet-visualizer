adcc090333b1816d77f804ce80abf816de567e93
lucien martijn <lmartijn@sentillia.com>
2024-09-15 21:41:34 +0200

    build: add postgresql parser to build.sh

--- build.sh
+++ build.sh
@@ -8,4 +8,4 @@ cp ./node_modules/duckdb-async/node_modules/duckdb/lib/binding/duckdb.node ./out
 cp ./node_modules/parquet-wasm/node/parquet_wasm_bg.wasm ./out;
 
 esbuild ./src/extension.ts --bundle --outfile=out/extension.js --external:vscode --external:nock --external:aws-sdk --external:mock-aws-s3 --format=cjs --platform=node --minify
-esbuild ./src/worker.ts --bundle --outfile=out/worker.js --external:vscode --external:nock --external:aws-sdk --external:mock-aws-s3 --format=cjs --platform=node --minify
+esbuild ./src/worker.ts --bundle --outfile=out/worker.js --external:vscode --external:nock --external:aws-sdk --external:mock-aws-s3 --external:pgsql-parser --format=cjs --platform=node --minify
