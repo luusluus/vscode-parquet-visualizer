f0caca44d9e25901902dc1a025e3908bbe3a1d77
lucien martijn <lmartijn@sentillia.com>
2024-11-09 13:31:34 +0100

    ci: disable publishing to marketplace

--- publish.release.config.js
+++ publish.release.config.js
@@ -3,13 +3,13 @@ module.exports = {
     plugins: [
       '@semantic-release/commit-analyzer',
       '@semantic-release/release-notes-generator',
-      [
-        'semantic-release-vsce',
-        {
-          packageVsix: false,
-          publishPackagePath: '*/*.vsix',
-        },
-      ],
+    //   [
+    //     'semantic-release-vsce',
+    //     {
+    //       packageVsix: false,
+    //       publishPackagePath: '*/*.vsix',
+    //     },
+    //   ],
       [
         '@semantic-release/github',
         {
