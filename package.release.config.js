150bc9b1943770bf29f69ca7c401e7a16e05aab6
lucien martijn <lmartijn@sentillia.com>
2024-11-09 13:03:33 +0100

    ci: add package and publish release config semantic-release

---
+++ package.release.config.js
@@ -0,0 +1,15 @@
+// package.release.config.js
+module.exports = {
+    plugins: [
+      '@semantic-release/commit-analyzer',
+      '@semantic-release/release-notes-generator',
+      [
+        'semantic-release-vsce',
+        {
+          packageVsix: true,
+          publish: false, // no-op since we use semantic-release-stop-before-publish
+        },
+      ],
+      'semantic-release-stop-before-publish',
+    ],
+  };
