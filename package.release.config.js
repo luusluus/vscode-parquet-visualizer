536d490be0bcc82ffd19ea18171e4dd3f9019894
lucien martijn <lmartijn@sentillia.com>
2024-11-09 14:15:42 +0100

    ci: ignore all commits

--- package.release.config.js
+++ package.release.config.js
@@ -1,13 +1,19 @@
 // package.release.config.js
 module.exports = {
     plugins: [
-      [
-        'semantic-release-vsce',
-        {
-          packageVsix: true,
-          publish: false, // no-op since we use semantic-release-stop-before-publish
-        },
-      ],
-      'semantic-release-stop-before-publish',
+        [
+            '@semantic-release/commit-analyzer',
+            {
+                releaseRules: [{ type: '*', release: false }], // Ignore all commits
+            },
+        ],
+        [
+            'semantic-release-vsce',
+            {
+                packageVsix: true,
+                publish: false, // no-op since we use semantic-release-stop-before-publish
+            },
+        ],
+        'semantic-release-stop-before-publish',
     ],
-  };
+};
