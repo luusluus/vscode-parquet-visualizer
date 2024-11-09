a2993107b72f69f78a2aa2366dbc5d2f2c6218ba
lucien martijn <lmartijn@sentillia.com>
2024-11-09 14:18:16 +0100

    ci: add back analyzer

--- package.release.config.js
+++ package.release.config.js
@@ -1,19 +1,14 @@
 // package.release.config.js
 module.exports = {
-    plugins: [
-        [
-            '@semantic-release/commit-analyzer',
-            {
-                releaseRules: [{ type: '*', release: false }], // Ignore all commits
-            },
-        ],
-        [
-            'semantic-release-vsce',
-            {
-                packageVsix: true,
-                publish: false, // no-op since we use semantic-release-stop-before-publish
-            },
-        ],
-        'semantic-release-stop-before-publish',
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    [
+      'semantic-release-vsce',
+      {
+        packageVsix: true,
+        publish: false, // no-op since we use semantic-release-stop-before-publish
+      },
     ],
+    'semantic-release-stop-before-publish',
+  ],
 };
