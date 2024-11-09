fb337570863de24908a7a401ba0ac76c6864d6c2
lucien martijn <lmartijn@sentillia.com>
2024-11-09 15:02:12 +0100

    ci: add '@semantic-release/git' and specify changelogFile location

--- publish.release.config.js
+++ publish.release.config.js
@@ -3,7 +3,13 @@ module.exports = {
     plugins: [
       '@semantic-release/commit-analyzer',
       '@semantic-release/release-notes-generator',
-      '@semantic-release/changelog',
+      [
+        '@semantic-release/changelog',
+        {
+          changelogFile: "CHANGELOG.md"
+        }
+      ],
+      '@semantic-release/git',
     //   [
     //     'semantic-release-vsce',
     //     {
