338fb135f2aa2995240ea2064cb93c279c73e867
lucien martijn <lmartijn@sentillia.com>
2024-11-11 00:41:16 +0100

    ci: pass secret to esbuild

--- build.js
+++ build.js
@@ -1,7 +1,12 @@
 const { exec } = require('child_process');
 
 const command = process.platform === 'win32' ? 'build.bat' : './build.sh';
-exec(command, (error, stdout, stderr) => {
+
+const envVars = {
+    ...process.env,
+    AZURE_APP_INSIGHTS_CONNECTION_STRING: process.env.AZURE_APP_INSIGHTS_CONNECTION_STRING,
+};
+exec(command, {env: envVars}, (error, stdout, stderr) => {
     if (error) {
         console.error(`Error: ${error.message}`);
         return;
