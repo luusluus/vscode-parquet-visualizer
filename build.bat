277d5afa3a2ab915493ec38a11effbcae03dd874
lucien martijn <lmartijn@sentillia.com>
2024-11-16 13:13:16 +0100

    chore: disable esbuild extension.ts

--- build.bat
+++ build.bat
@@ -6,12 +6,13 @@ mkdir out
 mkdir out\binding
 
 @REM REM Copy required files
+echo Copy required files
 copy /y node_modules\duckdb-async\node_modules\duckdb\lib\binding\duckdb.node out\binding\
 copy /y node_modules\parquet-wasm\node\parquet_wasm_bg.wasm out\
 
 @REM Run esbuild for extension.ts
 echo Run esbuild for extension.ts
-esbuild src\extension.ts --bundle --outfile=out\extension.js --external:vscode --external:nock --external:aws-sdk --external:mock-aws-s3 --format=cjs --platform=node --minify --define:process.env.AZURE_APP_INSIGHTS_CONNECTION_STRING="\"%AZURE_APP_INSIGHTS_CONNECTION_STRING%\""
+@REM esbuild src\extension.ts --bundle --outfile=out\extension.js --external:vscode --external:nock --external:aws-sdk --external:mock-aws-s3 --format=cjs --platform=node --minify --define:process.env.AZURE_APP_INSIGHTS_CONNECTION_STRING="\"%AZURE_APP_INSIGHTS_CONNECTION_STRING%\""
 
 @REM Run esbuild for worker.ts
 echo Run esbuild for worker.ts
