bb5d65428c73c995d90293cde0c967869040c1a6
lucien martijn <lmartijn@sentillia.com>
2024-11-16 12:45:23 +0100

    chore: add prints to bat script

--- build.bat
+++ build.bat
@@ -10,7 +10,9 @@ copy /y node_modules\duckdb-async\node_modules\duckdb\lib\binding\duckdb.node ou
 copy /y node_modules\parquet-wasm\node\parquet_wasm_bg.wasm out\
 
 REM Run esbuild for extension.ts
+echo Run esbuild for extension.ts
 esbuild src\extension.ts --bundle --outfile=out\extension.js --external:vscode --external:nock --external:aws-sdk --external:mock-aws-s3 --format=cjs --platform=node --minify --define:process.env.AZURE_APP_INSIGHTS_CONNECTION_STRING="\"%AZURE_APP_INSIGHTS_CONNECTION_STRING%\""
 
 REM Run esbuild for worker.ts
+echo Run esbuild for worker.ts
 esbuild src\worker.ts --bundle --outfile=out\worker.js --external:vscode --external:nock --external:aws-sdk --external:mock-aws-s3 --format=cjs --platform=node --minify
