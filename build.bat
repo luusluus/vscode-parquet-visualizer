4aa528f5f3056deae20ee2a8bb9309d505353014
lucien martijn <lmartijn@sentillia.com>
2024-11-16 13:00:20 +0100

    chore: remove all commands

--- build.bat
+++ build.bat
@@ -3,20 +3,20 @@
 echo Running build.bat
 
 echo clearing previous build
-REM Clear previous build
-rmdir /s /q out
-echo making new build folder
-mkdir out
-mkdir out\binding
+@REM REM Clear previous build
+@REM rmdir /s /q out
+@REM echo making new build folder
+@REM mkdir out
+@REM mkdir out\binding
 
-REM Copy required files
-copy /y node_modules\duckdb-async\node_modules\duckdb\lib\binding\duckdb.node out\binding\
-copy /y node_modules\parquet-wasm\node\parquet_wasm_bg.wasm out\
+@REM REM Copy required files
+@REM copy /y node_modules\duckdb-async\node_modules\duckdb\lib\binding\duckdb.node out\binding\
+@REM copy /y node_modules\parquet-wasm\node\parquet_wasm_bg.wasm out\
 
-REM Run esbuild for extension.ts
-REM echo Run esbuild for extension.ts
-REM esbuild src\extension.ts --bundle --outfile=out\extension.js --external:vscode --external:nock --external:aws-sdk --external:mock-aws-s3 --format=cjs --platform=node --minify --define:process.env.AZURE_APP_INSIGHTS_CONNECTION_STRING="\"%AZURE_APP_INSIGHTS_CONNECTION_STRING%\""
+@REM Run esbuild for extension.ts
+@REM echo Run esbuild for extension.ts
+@REM esbuild src\extension.ts --bundle --outfile=out\extension.js --external:vscode --external:nock --external:aws-sdk --external:mock-aws-s3 --format=cjs --platform=node --minify --define:process.env.AZURE_APP_INSIGHTS_CONNECTION_STRING="\"%AZURE_APP_INSIGHTS_CONNECTION_STRING%\""
 
-REM Run esbuild for worker.ts
-REM echo Run esbuild for worker.ts
-REM esbuild src\worker.ts --bundle --outfile=out\worker.js --external:vscode --external:nock --external:aws-sdk --external:mock-aws-s3 --format=cjs --platform=node --minify
+@REM Run esbuild for worker.ts
+@REM echo Run esbuild for worker.ts
+@REM esbuild src\worker.ts --bundle --outfile=out\worker.js --external:vscode --external:nock --external:aws-sdk --external:mock-aws-s3 --format=cjs --platform=node --minify
