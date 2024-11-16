b3fa986f44032d7873ca95f5f7a259ffbb6c8ae9
lucien martijn <lmartijn@sentillia.com>
2024-11-16 12:32:19 +0100

    chore: add contents of checked out files

--- build.js
+++ build.js
@@ -2,6 +2,19 @@ const path = require('path');
 const { exec } = require('child_process');
 
 console.log(`Current working directory: ${process.cwd()}`);
+
+fs.readdir(__dirname, (err, files) => {
+    if (err) {
+        console.error('Error reading directory:', err.message);
+        return;
+    }
+
+    console.log('Contents of the directory:');
+    files.forEach(file => {
+        console.log(file);
+    });
+});
+
 console.log(`${path.join(__dirname, 'build.bat')}`);
 
 const command = process.platform === 'win32' ? path.join(__dirname, 'build.bat') : './build.sh';
