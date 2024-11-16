c58f9c5f928f3ce5aa627c6a15956f139ddf50be
lucien martijn <lmartijn@sentillia.com>
2024-11-16 13:26:47 +0100

    chore: try build worker in separate process

--- build.bat
+++ build.bat
@@ -1,22 +1,12 @@
 @echo on
-setlocal enabledelayedexpansion
-
-echo Running build.bat
 
 mkdir out
 mkdir out\binding
 
 @REM REM Copy required files
-echo Copy required files
 copy /y node_modules\duckdb-async\node_modules\duckdb\lib\binding\duckdb.node out\binding\
 copy /y node_modules\parquet-wasm\node\parquet_wasm_bg.wasm out\
 
 @REM Run esbuild for extension.ts
 echo Run esbuild for extension.ts
 esbuild src\extension.ts --bundle --outfile=out\extension.js --external:vscode --external:nock --external:aws-sdk --external:mock-aws-s3 --format=cjs --platform=node --minify --define:process.env.AZURE_APP_INSIGHTS_CONNECTION_STRING="\"%AZURE_APP_INSIGHTS_CONNECTION_STRING%\""
-
-if errorlevel 1 set errorlevel=0
-
-@REM Run esbuild for worker.ts
-echo Run esbuild for worker.ts
-esbuild src\worker.ts --bundle --outfile=out\worker.js --external:vscode --external:nock --external:aws-sdk --external:mock-aws-s3 --format=cjs --platform=node --minify
--- build.js
+++ build.js
@@ -1,19 +1,48 @@
 const { exec } = require('child_process');
 
-const command = process.platform === 'win32' ? 'build.bat' : './build.sh';
-
 const envVars = {
     ...process.env,
     AZURE_APP_INSIGHTS_CONNECTION_STRING: process.env.AZURE_APP_INSIGHTS_CONNECTION_STRING,
 };
-exec(command, {env: envVars}, (error, stdout, stderr) => {
-    if (error) {
-        console.error(`Error: ${error.message}`);
-        process.exit(1);
-    }
-    if (stderr) {
-        console.error(`Stderr: ${stderr}`);
-        process.exit(1);
-    }
-    console.log(`Stdout: ${stdout}`);
-});
+
+if (process.platform !== 'win32') {
+
+    exec('./build.sh', {env: envVars}, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Error: ${error.message}`);
+            process.exit(1);
+        }
+        if (stderr) {
+            console.error(`Stderr: ${stderr}`);
+            process.exit(1);
+        }
+        console.log(`Stdout: ${stdout}`);
+    });
+} else {
+
+    console.log("executing build.bat");
+    exec('build.bat', {env: envVars}, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Error build extension: ${error.message}`);
+            process.exit(1);
+        }
+        if (stderr) {
+            console.error(`Stderr build extension: ${stderr}`);
+            process.exit(1);
+        }
+        console.log(`Stdout build extension: ${stdout}`);
+    });
+
+    console.log("executing build_worker.bat");
+    exec('build_worker.bat', {env: envVars}, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Error build worker: ${error.message}`);
+            process.exit(1);
+        }
+        if (stderr) {
+            console.error(`Stderr build worker: ${stderr}`);
+            process.exit(1);
+        }
+        console.log(`Stdout build worker: ${stdout}`);
+    });
+}
