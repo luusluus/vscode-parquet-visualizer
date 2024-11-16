99d2d52301abd2ed17d3445e0b3baa2529776264
lucien martijn <lmartijn@sentillia.com>
2024-11-16 13:37:28 +0100

    chore: wrap code in async iffe

--- build.js
+++ build.js
@@ -1,32 +1,35 @@
-const { exec } = require('child_process');
+(async function () {
 
-const envVars = {
-    ...process.env,
-    AZURE_APP_INSIGHTS_CONNECTION_STRING: process.env.AZURE_APP_INSIGHTS_CONNECTION_STRING,
-};
+    const { exec } = require('child_process');
 
-function execCommand(command, options) {
-    return new Promise((resolve, reject) => {
-        exec(command, options, (error, stdout, stderr) => {
-            if (error) {
-                console.error(`Error executing ${command}: ${error.message}`);
-                return reject(error);
-            }
-            if (stderr) {
-                console.error(`Stderr executing ${command}: ${stderr}`);
-            }
-            console.log(`Stdout executing ${command}: ${stdout}`);
-            resolve();
+    const envVars = {
+        ...process.env,
+        AZURE_APP_INSIGHTS_CONNECTION_STRING: process.env.AZURE_APP_INSIGHTS_CONNECTION_STRING,
+    };
+
+    function execCommand(command, options) {
+        return new Promise((resolve, reject) => {
+            exec(command, options, (error, stdout, stderr) => {
+                if (error) {
+                    console.error(`Error executing ${command}: ${error.message}`);
+                    return reject(error);
+                }
+                if (stderr) {
+                    console.error(`Stderr executing ${command}: ${stderr}`);
+                }
+                console.log(`Stdout executing ${command}: ${stdout}`);
+                resolve();
+            });
         });
-    });
-}
+    }
 
+    if (process.platform !== 'win32') {
+        await execCommand('./build.sh', {env: envVars});
+    } else {
+        console.log("executing build.bat");
+        await execCommand('build.bat', { env: envVars });
+        console.log("executing build_worker.bat");
+        await execCommand('build_worker.bat', { env: envVars });
+    }
 
-if (process.platform !== 'win32') {
-    await execCommand('./build.sh', {env: envVars});
-} else {
-    console.log("executing build.bat");
-    await execCommand('build.bat', { env: envVars });
-    console.log("executing build_worker.bat");
-    await execCommand('build_worker.bat', { env: envVars });
-}
+})();
