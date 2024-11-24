4642cba2f3f71ac5d3e2a28a55728c05fec852a5
lucien martijn <lmartijn@sentillia.com>
2024-11-24 21:48:03 +0100

    refactor: add FILENAME_SHORTNAME_EXTENSION_MAPPING constant

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,17 @@
-export const FILENAME_EXTENSION_FULLNAME_MAPPING: Record<string, string> = {
+export const FILENAME_SHORTNAME_FULLNAME_MAPPING: Record<string, string> = {
     'csv': 'Comma Separated Values file',
     'json': 'JavaScript Object Notation file',
     'parquet': 'Parquet file',
     'ndjson': 'Newline Delimited JSON file',
     'excel': 'Microsoft Excel file'
+};
+
+export const FILENAME_SHORTNAME_EXTENSION_MAPPING: Record<string, string> = {
+    'csv': 'csv',
+    'json': 'json',
+    'parquet': 'parquet',
+    'ndjson': 'ndjson',
+    'excel': 'xlsx'
 };
 
 export const REQUEST_SOURCE_DATA_TAB = 'dataTab';
