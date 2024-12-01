5e96b21d0f7df3027974aebe6d45338ade4a4c05
lucien martijn <lmartijn@sentillia.com>
2024-12-01 23:10:42 +0100

    build: deliberately fail batch script

--- build.bat
+++ build.bat
@@ -1,14 +1,18 @@
 @echo on
 
+exit /b 1
+
 mkdir out
 mkdir out\binding
 
 @REM REM Copy required files
 copy /y node_modules\duckdb-async\node_modules\duckdb\lib\binding\duckdb.node out\binding\
+if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
+
 copy /y node_modules\parquet-wasm\node\parquet_wasm_bg.wasm out\
+if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 
 @REM Run esbuild for extension.ts
 echo Run esbuild for extension.ts
 esbuild src\extension.ts --bundle --outfile=out\extension.js --external:vscode --external:nock --external:aws-sdk --external:mock-aws-s3 --format=cjs --platform=node --minify --define:process.env.AZURE_APP_INSIGHTS_CONNECTION_STRING="\"%AZURE_APP_INSIGHTS_CONNECTION_STRING%\""
-
-exit /b 1
+if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
